b68ab7c49867f011bb87c76ea1db18f1191f74e8
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2022-09-05 00:01:34 +0300

    Доделан инпут загрузки файлов

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 const mainMenu = document.querySelector("#drop-down-navigation-menu");
 const menuArrows = document.querySelector(".nav > li:nth-child(2)");
 const menuArrowActive = document.querySelector("#menu-arrow");
-/* menuArrows.addEventListener("click", (event) => {
-  mainMenu.style.visibility == "visible"
-    ? (mainMenu.style.visibility = "hidden")
-    : (mainMenu.style.visibility = "visible");
-  event.preventDefault();
-}); */
+const loadFile = document.querySelector("#load-file");
+const inputFile = document.querySelector("#input-file");
+const inputFileName = document.querySelector("#input-file-name");
 
 menuArrows.onclick = () => {
   mainMenu.classList.toggle("drop-down-navigation-menu-hidden");
   mainMenu.classList.toggle("drop-down-navigation-menu-visible");
   menuArrowActive.classList.toggle("menu-arrow");
-
-  /*   mainMenu.className == "drop-down-navigation-menu"
-    ? (mainMenu.className = "drop-down-navigation-menu-hidden")
-    : (mainMenu.className = "drop-down-navigation-menu"); */
 };
-//alert mainMenu;
+
+inputFile.onchange = function (e) {
+  const fileName =
+    this.files[0].name.length > 40
+      ? this.files[0].name.slice(0, 35) + "..." + this.files[0].name.slice(-5)
+      : this.files[0].name;
+
+  inputFileName.innerHTML = "Будет загружен файл: <br>" + fileName;
+};
