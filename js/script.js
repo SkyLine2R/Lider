a71d960c404755e2f51e4a3bb273675df6f9979a
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2022-09-15 08:50:57 +0300

    Добавлено меню с телефонами в хедере

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
+const phones = {
+  Москва: "+7 (499) 350-97-60",
+  "Санкт-Петербург": "+7 (500) 500-97-60",
+  Белгород: "+7 (200) 200-44-60",
+  "Ростов-на-Дону": "+7 (330) 540-44-60",
+};
+
 const mainMenu = document.querySelector("#drop-down-navigation-menu");
 const menuArrows = document.querySelector(".nav > li:nth-child(2)");
 const menuArrowActive = document.querySelector("#menu-arrow");
 const loadFile = document.querySelector("#load-file");
 const inputFile = document.querySelector("#input-file");
 const inputFileName = document.querySelector("#input-file-name");
+const citysPhonesField = document.querySelector(".citys-phones");
+const citysPhonesButton = document.querySelector("#citys-phones-button");
+const citysPhones = document.querySelector("#citys-phones");
+const ownerCityPhone = document.querySelector("#owner-city-phone");
+
+let htmlCitys = "<ul>";
+
+console.log(citysPhones);
+for (let item in phones) htmlCitys += `<li>${item}</li>`;
+
+citysPhones.innerHTML = htmlCitys + "</ul";
+
+citysPhonesButton.onclick = (e) => {
+  e.stopPropagation();
+  togglePhones();
+
+  document.onclick = (e) => {
+    const target = e.target;
+    const itsPhones = target == citysPhones || citysPhones.contains(target);
+
+    if (itsPhones) {
+      citysPhonesButton.innerHTML = target.innerHTML;
+      ownerCityPhone.innerHTML = phones[target.innerHTML];
+    }
+    togglePhones();
+  };
+};
 
 menuArrows.onclick = (e) => {
   e.stopPropagation();
@@ -17,7 +51,6 @@ menuArrows.onclick = (e) => {
       !itsMenu &&
       mainMenu.classList.contains("drop-down-navigation-menu-visible")
     ) {
-      e.stopPropagation();
       toggleMenu();
     }
   };
@@ -37,4 +70,9 @@ inputFile.onchange = function (e) {
 function toggleMenu() {
   mainMenu.classList.toggle("drop-down-navigation-menu-visible");
   menuArrowActive.classList.toggle("menu-arrow-active");
+}
+
+function togglePhones() {
+  citysPhones.classList.toggle("visible");
+  citysPhonesField.classList.toggle("visible");
 }
