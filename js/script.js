a50e884f15fb79396dfa1f5391aeb2d42e2a9b07
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2022-09-15 18:09:20 +0300

    Доработано открытие/закрытие меню

--- js/script.js
+++ js/script.js
@@ -5,55 +5,63 @@ const phones = {
   "Ростов-на-Дону": "+7 (330) 540-44-60",
 };
 
-const mainMenu = document.querySelector("#drop-down-navigation-menu");
-const menuArrows = document.querySelector(".nav > li:nth-child(2)");
-const menuArrowActive = document.querySelector("#menu-arrow");
-const loadFile = document.querySelector("#load-file");
-const inputFile = document.querySelector("#input-file");
-const inputFileName = document.querySelector("#input-file-name");
-const citysPhonesField = document.querySelector(".citys-phones");
-const citysPhonesButton = document.querySelector("#citys-phones-button");
-const citysPhones = document.querySelector("#citys-phones");
-const ownerCityPhone = document.querySelector("#owner-city-phone");
+const mainMenu = qS("#drop-down-navigation-menu");
+const menuArrows = qS(".nav > li:nth-child(2)");
+const menuArrowActive = qS("#menu-arrow");
+const loadFile = qS("#load-file");
+const inputFile = qS("#input-file");
+const inputFileName = qS("#input-file-name");
+const citysPhonesField = qS(".citys-phones");
+const citysPhonesButton = qS("#citys-phones-button");
+const citysPhones = qS("#citys-phones");
+const ownerCityPhone = qS("#owner-city-phone");
 
 let htmlCitys = "<ul>";
 
-console.log(citysPhones);
-for (let item in phones) htmlCitys += `<li>${item}</li>`;
+for (let item in phones) {
+  htmlCitys += `<li>${item}</li>`;
+}
 
 citysPhones.innerHTML = htmlCitys + "</ul";
 
-citysPhonesButton.onclick = (e) => {
-  e.stopPropagation();
+citysPhonesButton.onclick = () => {
   togglePhones();
 
-  document.onclick = (e) => {
-    const target = e.target;
-    const itsPhones = target == citysPhones || citysPhones.contains(target);
-
-    if (itsPhones) {
-      citysPhonesButton.innerHTML = target.innerHTML;
-      ownerCityPhone.innerHTML = phones[target.innerHTML];
-    }
+  citysPhones.onclick = (e) => {
+    //выбор города
+    citysPhonesButton.innerHTML = e.target.innerHTML;
+    ownerCityPhone.innerHTML = phones[e.target.innerHTML];
     togglePhones();
   };
 };
 
 menuArrows.onclick = (e) => {
-  e.stopPropagation();
   toggleMenu();
+};
 
-  document.onclick = (e) => {
-    //закрытие меню по щелчку вне меню
-    const target = e.target;
-    const itsMenu = target == mainMenu || mainMenu.contains(target);
-    if (
-      !itsMenu &&
-      mainMenu.classList.contains("drop-down-navigation-menu-visible")
-    ) {
-      toggleMenu();
-    }
-  };
+document.onclick = (e) => {
+  //закрытие любого меню по клике вне области меню
+  const target = e.target;
+
+  const itsPhones =
+    target == citysPhones ||
+    citysPhones.contains(target) ||
+    target == citysPhonesButton;
+
+  const itsMenu =
+    target == mainMenu ||
+    mainMenu.contains(target) ||
+    menuArrows.contains(target);
+
+  if (!itsPhones && citysPhones.classList.contains("visible")) {
+    togglePhones();
+  }
+  if (
+    !itsMenu &&
+    mainMenu.classList.contains("drop-down-navigation-menu-visible")
+  ) {
+    toggleMenu();
+  }
 };
 
 inputFile.onchange = function (e) {
@@ -75,4 +83,8 @@ function toggleMenu() {
 function togglePhones() {
   citysPhones.classList.toggle("visible");
   citysPhonesField.classList.toggle("visible");
+}
+
+function qS(properties) {
+  return document.querySelector(properties);
 }
